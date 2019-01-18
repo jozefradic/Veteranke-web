703330de6c56c432fa325e5d02618d2f25fe6ae3
Jakub Seliga <jakub.seliga@akademiasovy.sk>
2019-01-18 13:13:37 +0100

    SPA, add login/logout, dropdown, home and register component

--- FrontEnd/Veteran-SPA/src/app/nav/nav.component.ts
+++ FrontEnd/Veteran-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,14 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   // to store username and pass
   loginCredential: any = {};
-
-  constructor() { }
+// inject module auth service
+  constructor(private authService: AuthService ) { }
 
   ngOnInit() {
   }
-
+// send loginCredential(premenna z navbar) and send it to auth.service (tam však premennu volame model)
   login() {
-    console.log(this.loginCredential);
+    this.authService.login(this.loginCredential).subscribe(next => {
+      console.log('Logged in succesfully');
+    }, error => {
+      console.log('Failed to log in');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    // if something in token, returnt true, else false
+    return !!token;
+  }
+
+  loggOut() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
   }
 
 }
